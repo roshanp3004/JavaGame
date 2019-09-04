07fee1dac744f5c85f18760195b6b7ea996fa7db
Roshan Patil <54850227+roshanp3004@users.noreply.github.com>
2019-09-04 15:53:04 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public String play(int number) {
+
+        if (number == 0) throw new IllegalArgumentException("Number must not be 0");
+        if (number % 3 == 0) return "Fizz";
+        if (number % 5 == 0) return "Buzz";
+
+
+        return String.valueOf(number);
+    }
+
+    public  static void main(String args[])
+    {
+        Main obj= new Main();
+        System.out.println(obj.play(5));
+    }
+}
